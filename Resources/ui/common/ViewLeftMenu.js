e3b3644cec392e7e27777757079cf39f3547913f
Mike Ory <mory@livefyre.com>
2014-09-30 20:56:39 -0400

    open and closing working very nicely

--- Resources/ui/common/ViewLeftMenu.js
+++ Resources/ui/common/ViewLeftMenu.js
@@ -1,9 +1,6 @@
-//FirstView Component Constructor
-function ViewLeftMenu(vw_menuController, lightboxView) {
-    //create object instance, a parasitic subclass of Observable
+function ViewLeftMenu(vw_menuController, parentWindow) {
     var vw_leftMenu = Ti.UI.createView({
-        // top: 50,
-        // right: -100,
+        left: -Ti.Platform.displayCaps.platformWidth,  //offscreen
         height:Ti.Platform.displayCaps.platformHeight,
         width:Ti.Platform.displayCaps.platformWidth * .8,
         backgroundColor: 'black',
@@ -11,55 +8,72 @@ function ViewLeftMenu(vw_menuController, lightboxView) {
        // bubbleParent: false // so clicks on the window below don't bubble!
     });
     
-    vw_menuController.moving = false;
-    vw_menuController.axis = 0;
-    
-    var menuItems = Ti.UI.createLabel({
+    var lbl_menuItems = Ti.UI.createLabel({
         left:5,
         text:"Some Menu Items\nAndsomeMore\nAnd some others"
     });
     
-    vw_leftMenu.add(menuItems);
+    vw_leftMenu.add(lbl_menuItems);
     
-    vw_menuController.addEventListener('touchstart', function(e){
-        // Get starting horizontal position
-        e.source.axis = parseInt(e.x);
-    });
-    
-    vw_menuController.addEventListener('touchmove', function(e){
-  
+    function handleMoving(e){
         //stop if the menu is at 0
-        var loc = parseInt(e.x - vw_leftMenu.width);
-        console.log(loc);
+        var leftMenuViewWidth = vw_leftMenu.width;
+        var loc = parseInt(e.x - leftMenuViewWidth);
+
         if (loc <=0){
-            vw_leftMenu.left = parseInt(e.x - vw_leftMenu.width);
+            vw_leftMenu.left = parseInt(e.x - leftMenuViewWidth);
         }
+        
         //animate background opacity
-    });
-    vw_menuController.addEventListener('touchend', function(e){
-        console.log ('left: ', vw_leftMenu.left);
-        console.log('widht: ', vw_leftMenu.width);
-       //if it's open more than half way, open all the way, else send it back closed
-        if (-vw_leftMenu.left <= (vw_leftMenu.width / 2)){
-            //animate
+        var opac = Number((0.8 - (vw_leftMenu.left / -leftMenuViewWidth)).toFixed(2));
+        vw_lightBox.opacity = opac;
+    }
+    
+    function handleTouchEnd(e){
+        //if it's open more than 2/3 way, open all the way, else send it back closed
+        if (-vw_leftMenu.left <= (vw_leftMenu.width * .33)){
             vw_leftMenu.animate({
                 left : 0,
                 duration : 500
             });
-            
-            
+            vw_lightBox.opacity = 0.75;
+                       
         }else{
-            //animate
-            vw_leftMenu.animate({
-                left: -vw_leftMenu.width,
-                duration:500
-            });
-            lightboxView.opacity = .8;
+            slideClosed();
         }
-        
+    }
+    
+    vw_menuController.addEventListener('touchmove', handleMoving);
+    vw_menuController.addEventListener('touchend', handleTouchEnd);
+    vw_leftMenu.addEventListener('swipe', function(){
+        slideClosed();
     });
    
-
+   //create a greyed out layer to cover background window when menu is overlayed
+    var vw_lightBox = Ti.UI.createView({
+        height:Ti.UI.FILL,
+        width:Ti.UI.FILL,
+        left:0,
+        top:0,
+        backgroundColor:"#333333",
+        opacity:0,
+        zIndex:10
+    });
+    parentWindow.add(vw_lightBox);
+    
+    vw_lightBox.addEventListener('click',function(){
+        var position;
+        slideClosed();
+    });
+    
+    function slideClosed(){
+        vw_leftMenu.animate({
+            left: -vw_leftMenu.width,
+            duration:500
+        });
+        vw_lightBox.animate({opacity: 0, duration: 500});
+    }
+  
     return vw_leftMenu;
 }
 
--- Resources/ui/tablet/ApplicationWindow.js
+++ Resources/ui/tablet/ApplicationWindow.js
@@ -10,24 +10,7 @@ function ApplicationWindow() {
 	self.add(vw_menuBar);
 	
 	
-    //create a greyed out layer to cover background window when menu is overlayed
-    var vw_lightBox = Ti.UI.createView({
-        height:Ti.UI.FILL,
-        width:Ti.UI.FILL,
-        left:0,
-        top:0,
-        backgroundColor:"#333333",
-        opacity:0,
-        zIndex:10
-    });
-    self.add(vw_lightBox);
-    var opacityForBackground = 0;
     
-    vw_lightBox.addEventListener('click',function(){
-        var position;
-        slideClosed();
-        leftMenuAnimate();
-    });
 	
 	//create a transparent view that is 20 pix wide and runs down the left edge to control sliding a full view in and out
 	var vw_menuController = Ti.UI.createView({
@@ -37,41 +20,42 @@ function ApplicationWindow() {
 	    backgroundColor: "transparent",
 	    zIndex:20
 	});
-	
-	function slideOpen() {
-	    position = 0;
-        vw_menuController.isVisible = 1;
-        vw_menuController.width = vw_leftMenu.width;
-        opacityForBackground = .75;
-	}
-	
-	function slideClosed(){
-	    position = -Ti.Platform.displayCaps.platformWidth + 20;
-        vw_menuController.isVisible = 0;
-        vw_menuController.width = 20;
-        opacityForBackground = 0;
-	}
-	
-	function leftMenuAnimate(){
-	    vw_leftMenu.animate({
-            left: position,
-            backgroundColor : vw_menuController.isVisible ? "white" : "transparent",
-            speed:800
-        });
-        
-        vw_lightBox.animate({
-            opacity : opacityForBackground,
-            speed: 700
-        });
-	}
+// 	
+	// function slideOpen() {
+	    // position = 0;
+        // vw_menuController.isVisible = 1;
+        // vw_menuController.width = vw_leftMenu.width;
+        // opacityForBackground = .75;
+	// }
+// 	
+	// function slideClosed(){
+	    // position = -Ti.Platform.displayCaps.platformWidth + 20;
+        // vw_menuController.isVisible = 0;
+        // vw_menuController.width = 20;
+        // opacityForBackground = 0;
+	// }
+// 	
+	// function leftMenuAnimate(){
+	    // vw_leftMenu.animate({
+            // left: position,
+            // backgroundColor : vw_menuController.isVisible ? "white" : "transparent",
+            // speed:800
+        // });
+//         
+        // vw_lightBox.animate({
+            // opacity : opacityForBackground,
+            // speed: 700
+        // });
+	// }
 	
 	self.add(vw_menuController);
 	vw_menuController.isVisible = 0; //custom property to track view toggle
+	
 	//here's a fullscreen view that will be loaded by the vw_menuController below
     //here's a view that is full screen that slides in from the left
     var ViewLeftMenu = require('ui/common/ViewLeftMenu'); 
-    var vw_leftMenu = new ViewLeftMenu(vw_menuController, vw_lightBox); //pass in the parent window
-    vw_leftMenu.left = -Ti.Platform.displayCaps.platformWidth;  //offscreen
+    var vw_leftMenu = new ViewLeftMenu(vw_menuController, self); //pass in the parent window
+    
     self.add(vw_leftMenu);
 
 
